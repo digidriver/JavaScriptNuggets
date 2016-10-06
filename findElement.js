58c3223ddbfdb3d94405910500711a1b34be7edf
digidriver <digidriver@users.noreply.github.com>
2016-10-06 15:16:25 -0400

    findElement.js created

---
+++ findElement.js
@@ -0,0 +1,30 @@
+// Function findElement
+// Return first element in array that passes a truth test
+// Array: first argument
+// Truth test: second argument
+
+function findElement(arr, func) {
+  var result = 0;
+  
+  // Convert arguments list into array
+  var prototypeArray = arr.slice.call(arguments);
+  
+  // Harvest array and remaining argument details
+  var actualArray = arguments[0];
+  var truthTest = arguments[1];
+  
+  // Use a filter to sweep array and find first truthy match
+  result = actualArray.filter(willItTruthy, truthTest)[0];
+  return result;
+}
+
+// Helper function
+function willItTruthy(item){
+  if (this(item) === false){
+    return undefined;
+  } else {
+    return item;
+  }
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
